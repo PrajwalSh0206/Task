d9d6ae871937c9fdd29f430adb5442abca973adb
Munna0206 <prajwalshetty57@gmail.com>
2021-02-16 13:55:25 +0530

    navbar

--- task 2/tailwind.config.js
+++ task 2/tailwind.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-  purge: [],
+  purge: [ './*.html',
+  './*.js',],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {
-      backgroundColor:['group-hover'],
+      backgroundColor:['group-hover','active'],
       textColor:['group-hover'],
-      padding:['hover'],
-      margin:['hover']
+      padding:['group-hover','hover'],
+      borderWidth: ['hover', 'focus'],
+      fontWeight: ['hover', 'focus'],
+      display: ['hover', 'group-hover'],
+      borderRadius: ['hover', 'focus'],
     },
   },
   plugins: [],
