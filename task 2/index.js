73c91b86005c3ed668493e5178983e6ba408d9ef
PrajwalSh0206 <prajwalshetty57@gmail.com>
2021-02-18 15:38:23 +0530

    sidebar

--- task 2/index.js
+++ task 2/index.js
@@ -8,3 +8,15 @@ $(window).scroll(function () {
 $('#scrolltop').click(function (e) { 
     $("html, body").animate({ scrollTop: 0 }, "slow");
 });
+
+$('#hamburger').click(function(e){
+    $('#sidebar').removeClass('-translate-x-full').addClass('translate-x-0 ')
+})
+
+$(document).mouseup(function(e) 
+{
+    if (!$('#sidebar').is(e.target) && $('#sidebar').has(e.target).length === 0) 
+    {
+        $('#sidebar').removeClass('translate-x-0').addClass('-translate-x-full ')
+    }
+})
